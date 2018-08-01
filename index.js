5b69ccc7d461aff12f4abfeeb9a2d0d4f4810147
Whitereal <White_real@mail.ru>
2018-08-01 18:22:16 +0300

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '533806561:AAFaVI3T0vGvm_48YCyJiZr75OMKv-j2J6E'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "HI,${msg.from.first_name}"`)
+})
