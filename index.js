1f69300351a6e7e49b33a62567c10e5705b53eca
Whitereal <White_real@mail.ru>
2018-08-02 18:45:43 +0300

    initial

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ bot.on('message', msg => {
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
     res.end('')
 })﻿
+
+var http = require("http");
+setInterval(function() {
+    http.get("https://whiteplaynovel.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
