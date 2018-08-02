26ccc89b155d0b6e1785254842b670d69c1c76c7
Whitereal <White_real@mail.ru>
2018-08-02 18:49:55 +0300

    initial

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const TOKEN = '533806561:AAFaVI3T0vGvm_48YCyJiZr75OMKv-j2J6E'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Привет,${msg.from.first_name} из прошлого. Чо, не ждал, что у меня всё получится? Ха-ха!`)
+    bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}! Ну, что, не ждал еще одну попытку?) `)
 })
 
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
