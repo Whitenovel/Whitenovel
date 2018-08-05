0a1d0bfe0044330be4fbd0b480829e439cd566a8
Whitereal <White_real@mail.ru>
2018-08-05 18:07:29 +0300

    initial

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const TOKEN = '533806561:AAFaVI3T0vGvm_48YCyJiZr75OMKv-j2J6E'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}! Ну, что, не ждал еще одну попытку?) `)
+    bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}! Я молчу `),
+    bot.send_photo(chat_id=chat_id, photo='\heroku_bot\Photos\1.png')
 })
 
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
     res.end('')
-})﻿
+})
 
 var http = require("http");
-setInterval(function() {
-    http.get("https://whiteplaynovel.herokuapp.com/");
-}, 300000); // every 5 minutes (300000)
+setInterval(function()
+     {
+        http.get("https://whiteplaynovel.herokuapp.com/");
+     }, 
+     300000); // every 5 minutes (300000)
