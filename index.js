ea1b5abff3929d3e98ed3647948b67ae03f3f5b3
Whitereal <White_real@mail.ru>
2018-08-05 23:41:05 +0300

    initial

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN = '533806561:AAFaVI3T0vGvm_48YCyJiZr75OMKv-j2J6E'
 
+const PHOTO1 = 'https://raw.githubusercontent.com/Whitenovel/Whitenovel/master/Photos/1.png' 
+const PHOTO2 = 'https://raw.githubusercontent.com/Whitenovel/Whitenovel/master/Photos/2.jpg' 
+const PHOTO3 = 'https://raw.githubusercontent.com/Whitenovel/Whitenovel/master/Photos/3.jpg' 
+const PHOTO4 = 'https://raw.githubusercontent.com/Whitenovel/Whitenovel/master/Photos/4.jpg' 
+const PHOTO5 = 'https://raw.githubusercontent.com/Whitenovel/Whitenovel/master/Photos/5.jpg' 
+
+var Photos = [PHOTO1, PHOTO2, PHOTO3,PHOTO4,PHOTO5];
+
 const bot = new TelegramBot(TOKEN, {polling: true})
 
+
+
 bot.on('message', msg => {
+    var rand = Photos[Math.floor(Math.random() * Photos.length)];
     bot.sendMessage(msg.chat.id, `8Привет, ${msg.from.first_name}! Я молчу `),
     bot.sendMessage(msg.chat.id, `6Привет, ${msg.from.first_name}! молчу опять `),
-    bot.sendPhoto(msg.chat.id, photo='https://raw.githubusercontent.com/Whitenovel/Whitenovel/master/Photos/1.png'),
-    bot.send_photo(msg.chat.id, photo='https://raw.githubusercontent.com/Whitenovel/Whitenovel/master/Photos/1.png'),
+    bot.sendPhoto(msg.chat.id, photo=rand),
     bot.sendMessage(msg.chat.id, `3Привет, ${msg.from.first_name}! Это на всякий) `);
 
 })
