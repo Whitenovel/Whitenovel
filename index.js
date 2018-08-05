73b8b857090176d2d7f3fd6508590c7fcc33641c
Whitereal <White_real@mail.ru>
2018-08-05 18:27:28 +0300

    initial

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const TOKEN = '533806561:AAFaVI3T0vGvm_48YCyJiZr75OMKv-j2J6E'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `2Привет, ${msg.from.first_name}! Я молчу `),
-    bot.send_photo(chat_id=chat_id, photo='https://github.com/Whitenovel/Whitenovel/blob/master/Photos/1.png')
+    bot.sendMessage(msg.chat.id, `3Привет, ${msg.from.first_name}! Я молчу `),
+    bot.send_photo(chat_id=chat_id, photo='https://raw.githubusercontent.com/Whitenovel/Whitenovel/master/Photos/1.png')
 })
 
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
