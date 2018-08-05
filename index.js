ad94737beb01e7e6de4b10203c2e268f2770f14a
Whitereal <White_real@mail.ru>
2018-08-05 18:16:42 +0300

    initial

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const TOKEN = '533806561:AAFaVI3T0vGvm_48YCyJiZr75OMKv-j2J6E'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}! Я молчу `),
-    bot.send_photo(chat_id=chat_id, photo='\heroku_bot\Photos\1.png')
+    bot.sendMessage(msg.chat.id, `2Привет, ${msg.from.first_name}! Я молчу `),
+    bot.send_photo(chat_id=chat_id, photo='\Photos\1.png')
 })
 
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
